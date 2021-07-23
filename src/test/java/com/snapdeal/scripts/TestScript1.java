07c8c48f514d71e58b4bee81421105bd977b2453
Namit Aggarwal <qa.namitaggarwal@gmail.com>
2021-07-24 02:25:15 +0530

    Third Commit

--- src/test/java/com/snapdeal/scripts/TestScript1.java
+++ src/test/java/com/snapdeal/scripts/TestScript1.java
@@ -3,8 +3,8 @@ package com.snapdeal.scripts;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import javax.mail.MessagingException;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -38,24 +38,15 @@ public class TestScript1{
 			WebElement SearchButton = driver.findElement(By.xpath("//span[@class='searchTextSpan']"));
 			SearchButton.click();
 			WebElement SortByDropDown = driver.findElement(By.xpath("//i[@class='sd-icon sd-icon-expand-arrow sort-arrow']"));
-//			if(SortByDropDown.isDisplayed()) {
-				SortByDropDown.click();
-//			}
+			SortByDropDown.click();
 			WebElement PriceLowToHigh = driver.findElement(By.xpath("//*[@id=\"content_wrapper\"]/div[7]/div[5]/div[3]/div[1]/div/div[2]/ul/li[3]"));
-//			if(PriceLowToHigh.isDisplayed()) {
-				PriceLowToHigh.click();
-//			}
-			String price;
-//			List<WebElement> AllProductImageList = driver.findElements(By.xpath("//p[@class='product-title ']/../../../../div[@class='product-tuple-image ']"));
-//			System.out.println("Total Number of Products Image List: "+AllProductImageList.size());
+			PriceLowToHigh.click();
+			String price = null;
+			Thread.sleep(1000);
 			List<WebElement> AllProductName = driver.findElements(By.xpath("//p[@class='product-title ']"));
 			System.out.println("Total Number of Products: "+AllProductName.size());
 			List<WebElement> AllProductPrice = driver.findElements(By.xpath("//span[@class='lfloat product-price']")); 
 			System.out.println("Total Number of Price: "+AllProductPrice.size());
-//			for (int j = 1; j < AllProductPrice.size(); j++) {
-//				price = AllProductPrice.get(j).getText();
-//				System.out.println(price);
-//				}
 			for(int i=0;i<AllProductName.size();i++) {
 				System.out.println(AllProductName.get(i).getText());
 			 }
@@ -67,9 +58,32 @@ public class TestScript1{
 				}
 			 }
 		    ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
-		    driver.switchTo().window(tabs2.get(1));
-		    driver.close();
 		    driver.switchTo().window(tabs2.get(0));
+		    driver.close();
+		    driver.switchTo().window(tabs2.get(1));
+		    String ProductName = driver.findElement(By.xpath("//*[@id=\"productOverview\"]/div[2]/div/div[1]/div[1]/div[1]/h1")).getText();
+		    String ProductPrice = driver.findElement(By.xpath("//*[@id=\"buyPriceBox\"]/div[2]/div[1]/div[1]/div[1]/span[1]/span")).getText();
+		    WebElement AddToCart = driver.findElement(By.xpath("//span[text()='add to cart']"));
+		    AddToCart.click();
+		    
+		    String ProductNameInCart = driver.findElement(By.xpath("//*[@id=\"cartProductContainer\"]/div/div[2]/div[1]/div/div[2]/span[1]/a")).getText();
+		    String ProductPriceInCartWithDigits = driver.findElement(By.xpath("//*[@id=\"cartProductContainer\"]/div/div[2]/div[2]/div/div[1]/div[2]/span")).getText();
+			String ProductPriceInCart = ProductPriceInCartWithDigits.replaceAll("[^0-9]", "");
+			System.out.println(ProductPriceInCart);
+			if(ProductNameInCart.equals(ProductName)) {
+		    	if(ProductPriceInCart.equals(ProductPrice)) {
+		    		System.out.println("Product Price is same as it was selected.");
+		    	}else {
+		    		System.out.println("Product Price has is difference.");
+			    	
+		    		
+//		    		StringUtils.difference(ProductPriceInCart,ProductPrice);
+		    		
+		    		
+		    	}
+		    }else {
+		    	System.out.println("Different Product has been added to cart. Product selected was: "+ProductName+" and Product displaying was: "+ProductNameInCart);
+		    }
 		}else {
 			System.out.println("Search text box is not displayed.");
 		}
